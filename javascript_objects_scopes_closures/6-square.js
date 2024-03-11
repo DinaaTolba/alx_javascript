4eee85b9ad8dd1e68164bbc57670989655b498c0
DinaTolba <magdydina2002@gmail.com>
2024-03-11 22:38:44 +0200

    task6

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+const SquareBase = require('./5-square');
+
+class Square extends SquareBase {
+    charPrint(c) {
+        if (c === undefined) {
+            c = 'X';
+        }
+        for (let i = 0; i < this.width; i++) {
+            let row = '';
+            for (let j = 0; j < this.width; j++) {
+                row += c;
+            }
+            console.log(row);
+        }
+    }
+}
+
+module.exports = Square;
