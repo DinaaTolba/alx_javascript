2973c37427960241b923826e9f9f446e46031bcd
DinaTolba <magdydina2002@gmail.com>
2024-03-11 21:42:18 +0200

    task0

---
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,7 @@
+// 0-javascript_is_amazing.js
+
+// Declare a constant variable named myVar and assign the value "JavaScript is amazing"
+const myVar = "JavaScript is amazing";
+
+// Use console.log() to print the value of myVar
+console.log(myVar);
