f69775ffef329394c9f155a106b676853df286d8
DinaTolba <magdydina2002@gmail.com>
2024-03-11 22:27:30 +0200

    task2

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,14 +1,11 @@
 class Rectangle {
     constructor(w, h) {
-      if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
-        this.width = undefined;
-        this.height = undefined;
-      } else {
+        if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
+            return {}; // returning an empty object as per the requirement
+        }
         this.width = w;
         this.height = h;
-      }
     }
-  }
-  
-  module.exports = Rectangle;
-  
+}
+
+module.exports = Rectangle;
