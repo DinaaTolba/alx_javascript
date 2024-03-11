bbade21a0b488675117a65486e713be22d4f8ace
DinaTolba <magdydina2002@gmail.com>
2024-03-11 23:18:21 +0200

    task3

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -1,26 +1,25 @@
-const axios = require('axios');
 const fs = require('fs');
+const request = require('request');
 
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-// Function to get the contents of the webpage and store it in a file
-const getRequestAndStore = async (url, filePath) => {
-    try {
-        const response = await axios.get(url);
-        const body = response.data;
-        
-        // Write the body response to the file
-        fs.writeFile(filePath, body, 'utf-8', (err) => {
-            if (err) {
-                console.error('Error writing to file:', err);
-                return;
-            }
-            console.log(`The body response has been saved to ${filePath}`);
-        });
-    } catch (error) {
-        console.error('Error:', error.message);
-    }
-};
+request(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
 
-getRequestAndStore(url, filePath);
+  if (response.statusCode !== 200) {
+    console.error('Status code:', response.statusCode);
+    return;
+  }
+
+  fs.writeFile(filePath, body, { encoding: 'utf-8' }, (err) => {
+    if (err) {
+      console.error('Error writing to file:', err);
+      return;
+    }
+    console.log(`File written successfully to ${filePath}`);
+  });
+});
